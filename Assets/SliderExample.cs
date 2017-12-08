b2d5a7e2f3828e7a016277204fe77268d26de1c2
Paul Sinnett <paul.sinnett@gmail.com>
2017-12-08 13:56:05 +0000

    Initial commit

---
+++ Assets/SliderExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderExample : MonoBehaviour
+{
+    Slider slider;
+
+    void Start()
+    {
+        // this script must be placed on the slider GameObject
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener((value) => ChangeValue(value));
+    }
+	
+    void ChangeValue(float value)
+    {
+        // You can do what you like with 'value' here.
+        // This code will happen every time the value in the slider changes.
+        Debug.Log(value);
+    }
+}
